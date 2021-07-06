8b2048741498423943babac45112397db888522f
lucifer2355 <dgajjar999@gmail.com>
2021-07-06 19:03:20 +0530

    fix graphql issue

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from "react-dom";
 import ApolloProvider from "./ApolloProvider";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(ApolloProvider, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>{ApolloProvider}</React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
