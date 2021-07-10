645ae5f037abd447088ec410bab555e04c94455a
“lucifer2355” <dgajjar999@gmail.com>
2021-07-10 13:13:27 +0530

    add protect route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 import { AuthProvider } from "./context/auth";
-import "semantic-ui-css/semantic.min.css";
+import AuthRoute from "./utils/AuthRoute";
 import MenuBar from "./components/MenuBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -16,8 +17,8 @@ function App() {
         <Container>
           <MenuBar />
           <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
+          <AuthRoute exact path='/login' component={Login} />
+          <AuthRoute exact path='/register' component={Register} />
         </Container>
       </Router>
     </AuthProvider>
