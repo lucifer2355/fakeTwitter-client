e54485fb988c05e6d39a7173c7905c5f35e5239e
“lucifer2355” <dgajjar999@gmail.com>
2021-07-10 19:14:13 +0530

    add transition effect to new post

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -30,6 +30,9 @@ const PostForm = () => {
       });
       values.body = "";
     },
+    onError(err) {
+      console.log("create post error", err);
+    },
   });
 
   function createPostCallback() {
@@ -37,21 +40,32 @@ const PostForm = () => {
   }
 
   return (
-    <Form onSubmit={onSubmit}>
-      <h2>Create a post:</h2>
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>Create a post:</h2>
 
-      <Form.Field>
-        <Form.Input
-          placeholder='Hi World!'
-          name='body'
-          onChange={onChange}
-          value={values.body}
-        />
-        <Button type='submit' color='teal'>
-          Submit
-        </Button>
-      </Form.Field>
-    </Form>
+        <Form.Field>
+          <Form.Input
+            placeholder='Hi World!'
+            name='body'
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type='submit' color='teal'>
+            Submit
+          </Button>
+        </Form.Field>
+      </Form>
+
+      {error && (
+        <div className='ui error message' style={{ marginBottom: 20 }}>
+          <ul className='list'>
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
