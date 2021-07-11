19a5a7fd4b610157e1e531fdcb13444b96f2071d
“lucifer2355” <dgajjar999@gmail.com>
2021-07-11 12:38:21 +0530

    add confrim model for delete post

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,24 +1,36 @@
-import React from "react";
-import { Button, Icon } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Button, Icon, Confirm } from "semantic-ui-react";
 import { useMutation } from "@apollo/client";
 import { DELETE_POST_MUTATION } from "../graphql/mutation/deletePost";
 
 const DeleteButton = ({ postId }) => {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
   const { deletePost } = useMutation(DELETE_POST_MUTATION, {
     variables: {
       postId,
     },
+    update() {
+      setConfirmOpen(false);
+    },
   });
 
   return (
-    <Button
-      as='div'
-      color='red'
-      floated='right'
-      onClick={() => console.log("Delete post")}
-    >
-      <Icon name='trash' style={{ margin: 0 }} />
-    </Button>
+    <>
+      <Button
+        as='div'
+        color='red'
+        floated='right'
+        onClick={() => console.log("Delete post")}
+      >
+        <Icon name='trash' style={{ margin: 0 }} />
+      </Button>
+      <Confirm
+        open={confirmOpen}
+        onCancel={() => setConfirmOpen(false)}
+        onConfirm={deletePost}
+      />
+    </>
   );
 };
 
