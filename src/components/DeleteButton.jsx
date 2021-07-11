9e9c92b9991a867c5c6e3ccd8e033dd341bcff7d
“lucifer2355” <dgajjar999@gmail.com>
2021-07-11 13:18:00 +0530

    delete post from graphql cache

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 import { Button, Icon, Confirm } from "semantic-ui-react";
 import { useMutation } from "@apollo/client";
+
 import { DELETE_POST_MUTATION } from "../graphql/mutation/deletePost";
+import { FETCH_POSTS_QUERY } from "../graphql/query/fetchPosts";
 
 const DeleteButton = ({ postId, callback }) => {
   const [confirmOpen, setConfirmOpen] = useState(false);
 
   const [deletePost] = useMutation(DELETE_POST_MUTATION, {
-    update() {
+    update(proxy) {
       setConfirmOpen(false);
+
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY,
+      });
+      let newData = data.getPosts.filter((p) => p.id !== postId);
+      proxy.writeQuery({
+        query: FETCH_POSTS_QUERY,
+        data: {
+          ...data,
+          getPosts: {
+            newData,
+          },
+        },
+      });
+
       if (callback) callback();
     },
     variables: {
